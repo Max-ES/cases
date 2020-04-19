152175838978484fdf5db49d154d2ae71567e462
Max <maxes2000@t-online.de>
2020-04-19 13:58:42 +0200

    Setup Angular Project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.sass']
 })
 export class AppComponent {
-  title = 'cases';
+  title = 'CASES GmbH';
+  public isMenuCollapsed = true;
 }
